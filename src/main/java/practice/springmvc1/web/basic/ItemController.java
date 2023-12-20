812a463463481f0a94f07910f0b5c46fdfd28b79
YongHyeon <pyh4616@naver.com>
2023-12-20 21:02:19 +0900

    아이템 정보 URL 매핑

--- src/main/java/practice/springmvc1/web/basic/ItemController.java
+++ src/main/java/practice/springmvc1/web/basic/ItemController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import practice.springmvc1.domain.item.Item;
 import practice.springmvc1.domain.item.ItemRepository;
@@ -22,5 +23,12 @@ public class ItemController {
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
         return "basic/items";
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
     }
 }
